c3cb589e9bdded439b4197940a790314d878ae0c
Michael Rommel <rommel@layer-7.net>
2023-05-20 02:30:46 +0200

    fix: make tmux compile on macOS

--- bootstrap/post-clone.sh
+++ bootstrap/post-clone.sh
@@ -1,11 +1,32 @@
-#! /bin/bash
+#! /usr/bin/env bash
+
+source "${HOME}/.homesick/helper.sh"
 
 TMUX_VERSION=$(tmux 2>/dev/null -V | sed -En "s/^tmux[^0-9]*([.0-9]+).*/\1/p")
-if [[ -z "${TMUX_VERSION}" || "$(echo "${TMUX_VERSION} < 3.5" | bc)" -eq 1 ]]; then
-	echo "Installing build requirements for tmux"
-	sudo apt-get -y update
-	sudo apt-get -y install build-essential autoconf automake pkg-config \
-		libevent-dev libncurses5-dev byacc
+if ! satisfied "3.5" "${TMUX_VERSION}"; then
+	if is_mac; then
+		# gawk for plugins, utf8proc neeeded on mac
+		desired=(autoconf automake pkg-config utf8proc gawk)
+		missing=()
+		check_brewed "missing" "${desired[@]}"
+		declare -p missing
+		if [[ "${#missing[@]}" -gt 0 ]]; then
+			echo "(brew) installing ${missing[*]}"
+			brew install "${missing[@]}"
+		fi
+	else
+		# gawk needed for plugins
+		desired=(build-essential autoconf automake pkg-config
+			libevent-dev libncurses5-dev byacc gawk)
+		missing=()
+		check_dpkged "missing" "${desired[@]}"
+		if [[ "${#missing[@]}" -gt 0 ]]; then
+			echo "(apt) installing ${missing[*]}"
+			sudo apt-get -y update
+			sudo apt-get -y install "${missing[@]}"
+		fi
+
+	fi
 	echo "Recompiling tmux"
 	mkdir -p "${HOME}/software"
 	cd "${HOME}/software" || exit
@@ -14,13 +35,19 @@ if [[ -z "${TMUX_VERSION}" || "$(echo "${TMUX_VERSION} < 3.5" | bc)" -eq 1 ]]; t
 	# optionally use a specific version
 	# git checkout 3.3a
 	sh autogen.sh
-	./configure --prefix="${HOME}/software/tmux"
+	if is_mac; then
+		FLAGS="--enable-utf8proc"
+		CPPFLAGS="-I/opt/homebrew/Cellar/utf8proc/2.8.0/include" \
+			LDFLAGS="-L/opt/homebrew/Cellar/utf8proc/2.8.0/lib" \
+			./configure ${FLAGS} --prefix="${HOME}/software/tmux"
+	else
+		./configure --prefix="${HOME}/software/tmux"
+	fi
 	make
 	make install
 fi
 
 echo "Configuring tmux plugins"
-sudo apt-get -y install gawk
 mkdir -p "${HOME}/.local/share/tmux/plugins"
 cd "${HOME}/.local/share/tmux/plugins" || exit
 git clone --depth=1 https://github.com/tmux-plugins/tpm "${HOME}/.local/share/tmux/plugins/tpm"
