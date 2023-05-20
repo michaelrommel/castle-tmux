20a2ac661c83d8ac11dcbc0b296e64377ca45919
Michael Rommel <rommel@layer-7.net>
2023-05-20 03:06:01 +0200

    fix: make tmux link with homebrew ncurses lib

--- bootstrap/post-clone.sh
+++ bootstrap/post-clone.sh
@@ -9,15 +9,14 @@ if ! satisfied "3.5" "${TMUX_VERSION}"; then
 		desired=(autoconf automake pkg-config utf8proc gawk)
 		missing=()
 		check_brewed "missing" "${desired[@]}"
-		declare -p missing
 		if [[ "${#missing[@]}" -gt 0 ]]; then
 			echo "(brew) installing ${missing[*]}"
 			brew install "${missing[@]}"
 		fi
 	else
 		# gawk needed for plugins
-		desired=(build-essential autoconf automake pkg-config \
-			libevent-dev libncurses5-dev libutf8proc-dev \
+		desired=(build-essential autoconf automake pkg-config
+			libevent-dev libncurses5-dev libutf8proc-dev
 			libutf8proc2 byacc gawk)
 		missing=()
 		check_dpkged "missing" "${desired[@]}"
@@ -38,8 +37,9 @@ if ! satisfied "3.5" "${TMUX_VERSION}"; then
 	sh autogen.sh
 	FLAGS="--enable-utf8proc"
 	if is_mac; then
-		CPPFLAGS="-I/opt/homebrew/Cellar/utf8proc/2.8.0/include" \
-			LDFLAGS="-L/opt/homebrew/Cellar/utf8proc/2.8.0/lib" \
+		export PKG_CONFIG_PATH="/opt/homebrew/opt/ncurses/lib/pkgconfig"
+		CPPFLAGS="-I/opt/homebrew/Cellar/utf8proc/2.8.0/include -I/opt/homebrew/opt/ncurses/include" \
+			LDFLAGS="-L/opt/homebrew/Cellar/utf8proc/2.8.0/lib -L/opt/homebrew/opt/ncurses/lib" \
 			./configure ${FLAGS} --prefix="${HOME}/software/tmux"
 	else
 		./configure ${FLAGS} --prefix="${HOME}/software/tmux"
@@ -51,4 +51,6 @@ fi
 echo "Configuring tmux plugins"
 mkdir -p "${HOME}/.local/share/tmux/plugins"
 cd "${HOME}/.local/share/tmux/plugins" || exit
-git clone --depth=1 https://github.com/tmux-plugins/tpm "${HOME}/.local/share/tmux/plugins/tpm"
+if [[ ! -d "tpm" ]]; then
+	git clone --depth=1 https://github.com/tmux-plugins/tpm "${HOME}/.local/share/tmux/plugins/tpm"
+fi
